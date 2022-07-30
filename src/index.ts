bab509fe8d9d9e3417e12eaf21c31062ef2d7203
trungvt <thanhtrungvo8401@gmail.com>
2022-07-30 23:58:06 +0700

    HUSKY: format and commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-console.log('Hello world')
+console.log('Hello world');
 
-let array = [1, 2, 3,4 ,5 ,55, 5];
+const array = [1, 2, 3, 4, 5, 55, 5];
 
 for (let i = 0; i < array.length; i++) {
-    console.log(i)
-  }
+  console.log(i);
+}
